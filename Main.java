8bf4308d72c52f96fdef06ff5f26fcd183362527
Gotoher <144687142+Gotoher@users.noreply.github.com>
2023-09-17 13:08:13 +0800

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        SimhashAlgoService simhashAlgoService = new SimhashAlgoService();
-        String result = simhashAlgoService.compare("He is a boy", "but she is a girl");
-        System.out.println(result);
-    }
-}
